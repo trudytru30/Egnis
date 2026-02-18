c020bb52ac47978661c2390a79e339489f51bc8a
trudytru30 <adriana.alamoclase@gmail.com>
2026-02-18 12:20:46 +0100

    characters base
    base .h enemigos/aliados/jugadorBase

---
+++ Egnis/Source/Egnis/Private/Ally.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ally.h"
+
+
+// Sets default values
+AAlly::AAlly()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAlly::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAlly::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AAlly::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
