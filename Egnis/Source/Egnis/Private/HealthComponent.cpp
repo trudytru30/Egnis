a1efdb05fc39c1105860b448f9363437e580c715
trudytru30 <adriana.alamoclase@gmail.com>
2026-02-20 09:10:47 +0100

    HealthComponent base
    Ya come daño y tiene vida pero solo hace lo basico

--- Egnis/Source/Egnis/Private/HealthComponent.cpp
+++ Egnis/Source/Egnis/Private/HealthComponent.cpp
@@ -7,30 +7,56 @@
 // Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
-
 
 // Called when the game starts
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	CurrentHealth = FMath::Clamp(CurrentHealth, 0.0f, MaxHealth);
+	BroadcastHealth();
 	
 }
 
-
-// Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                       FActorComponentTickFunction* ThisTickFunction)
+float UHealthComponent::ApplyDelta(float Delta)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (Delta == 0.0f)
+	{
+		return CurrentHealth;
+	}
 
-	// ...
+	const float OldHealth = CurrentHealth;
+	CurrentHealth  = FMath::Clamp(CurrentHealth+Delta,0.0f,MaxHealth);
+
+	const bool bChanged = !FMath::IsNearlyEqual(CurrentHealth,OldHealth);
+
+	if (bChanged)
+	{
+		BroadcastHealth();
+	}
+
+	//Si el delta es por daño y no por curacion
+	const bool bDamageApplied = (Delta < 0.0f) && (CurrentHealth < OldHealth);
+
+	//Si no nos mata aplicamos hitReact
+	if (bDamageApplied && CurrentHealth > 0.0f)
+	{
+		//queu visaul al jugador de daño
+	}
+
+	//Si nos mata aplicamos Death
+	if (OldHealth > 0.0f &&  CurrentHealth <=0.0f)
+	{
+		OnDeath.Broadcast();
+	}
+
+	return CurrentHealth;
 }
 
+
+void UHealthComponent::BroadcastHealth()
+{
+	OnHealthChanged.Broadcast(CurrentHealth,MaxHealth);
+}
--- Egnis/Source/Egnis/Public/HealthComponent.h
+++ Egnis/Source/Egnis/Public/HealthComponent.h
@@ -7,21 +7,47 @@
 #include "HealthComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChangedSignature,float,CurrentHealth,float,MaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FonDeathSignature);
+
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class EGNIS_API UHealthComponent : public UActorComponent
+class PELEAMELEE_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this component's properties
 	UHealthComponent();
+	
+	UFUNCTION(BlueprintCallable, Category="Health")
+	float ApplyDelta(float Delta);
+	
+	UFUNCTION(BlueprintCallable, Category="Health")
+	float GetCurrentHealt() {return CurrentHealth;}
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	float GetMaxHealt() {return MaxHealth;}
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	bool IsDead() {return CurrentHealth<=0.0f;}
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FOnHealthChangedSignature OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FonDeathSignature OnDeath;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Health")
+	float MaxHealth = 100;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Health")
+	float CurrentHealth = 100;
+
+private:
+	void BroadcastHealth();
 };
