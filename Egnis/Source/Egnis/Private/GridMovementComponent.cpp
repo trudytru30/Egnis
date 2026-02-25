12c174623e9f7d228933f15be5c10755035d1179
nicky <nickygrivera@hotmail.com>
2026-02-25 12:38:05 +0100

    Movement
    GridMovemenet Component+ Conexion con BP_Board,BP_BoardController y BP_Unit

---
+++ Egnis/Source/Egnis/Private/GridMovementComponent.cpp
@@ -0,0 +1,189 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridMovementComponent.h"
+#include "Engine.h"
+#include"Kismet/GameplayStatics.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UGridMovementComponent::UGridMovementComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	//PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+void UGridMovementComponent::EnsureBoardActor()
+{
+	if (BoardActor)
+	{
+		return;
+	}
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABoard::StaticClass(), FoundActors);
+
+	if (FoundActors.Num() > 0)
+	{
+		BoardActor=Cast<ABoard>(FoundActors[0]);//
+
+		UE_LOG(LogTemp,Warning, TEXT("Tablero asignado:%s"),*GetNameSafe(BoardActor));
+
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Tablero"));
+		}
+
+	}
+	else
+	{
+		UE_LOG(LogTemp,Error,TEXT("No se ha encontrado ningun tablero"));
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 4.0f, FColor::Red,TEXT("No se ha encontrado tablero"));
+				
+		}
+	}
+}
+
+//comprueba casilla si esta dentro del tablero 
+bool UGridMovementComponent::IsTileValid(const FTileCoord& T)
+{
+	if (!BoardActor)
+	{
+		return false;
+	}
+
+	return (T.X >= 0 && T.X < BoardActor->BoardSizeX && T.Y >= 0 && T.Y < BoardActor->BoardSizeY);
+}
+
+
+//calcula la actual
+bool UGridMovementComponent::getCurrentTile(FTileCoord& OutTile)
+{
+	if (!BoardActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No hay BoardActor asignado"));
+		return false;
+	}
+
+	return BoardActor->WorldPointToTile(GetOwner()->GetActorLocation(), OutTile);
+}
+
+//devuelve tods las casillas a las que se puede mover (8 dir hasra rango)
+void UGridMovementComponent::getRachableTiles(TArray<FTileCoord>& OutTiles)
+{
+	OutTiles.Reset();//.Empty();
+
+	EnsureBoardActor();
+
+	if (!BoardActor)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No hay tablero asignado"));
+		return;
+	}
+	FTileCoord CurrentTile;
+	if (!getCurrentTile(CurrentTile))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No se pudo obtener la casilla actual"));
+		return;
+	}
+
+	//definicion de 8 dir 
+	const TArray<FTileCoord> Directions = {
+		{1, 0}, {1, 1}, {0, 1}, {-1, 1},
+		{-1, 0}, {-1, -1}, {0, -1}, {1, -1}
+	};
+
+	for (const FTileCoord& Dir : Directions)
+	{
+		for (int32 Step = 1; Step <= moveRange; ++Step)
+		{
+			FTileCoord Target = { CurrentTile.X + Dir.X * Step, CurrentTile.Y + Dir.Y * Step };
+			if (IsTileValid(Target))
+			{
+				OutTiles.Add(Target);
+				//meter sist blqueos u obstaculos
+				/*if(IsTileBlocked(Target)){
+				 *break;
+				 *}
+				 **/
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+}
+
+bool UGridMovementComponent::canMoveToTile(const FTileCoord& Target)
+{
+	if (!BoardActor)
+	{
+		EnsureBoardActor();
+	}
+
+	if (!BoardActor)
+	{
+		return false;
+	}
+	if (!IsTileValid(Target))
+	{
+		return false;
+	}
+
+	//obtiene las casillas a las que se puede mover
+	TArray<FTileCoord> ReachableTiles;
+	getRachableTiles(ReachableTiles);
+
+	//comprueba si la casilla objetivo esta dentro
+	for (const FTileCoord& Tile : ReachableTiles)
+	{
+		if (Tile.X == Target.X && Tile.Y == Target.Y)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UGridMovementComponent::moveToTile(const FTileCoord& Target)
+{
+	EnsureBoardActor();
+
+	if (!BoardActor)
+	{
+		return false;
+	}
+
+	//comprueba limites
+	if (!IsTileValid(Target))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Casilla invalida (%d,%d)"), Target.X, Target.Y);
+		return false;
+	}
+
+	//comprueba que esta dentro del patron 
+	if (!canMoveToTile(Target))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Movimiento no permitido a (%d,%d)"), Target.X, Target.Y);
+		return false;
+	}
+	/*
+	 *Actualizar sist de ocupacion de casillas del tablero
+	 *liberar casilla anterior
+	 * marcar casilla nueva como ocupada
+	 */
+
+	//casilla a pos en juego
+	FVector Destination = BoardActor->TileToWorldCenter(Target);
+	//mover actor
+	GetOwner()->SetActorLocation(Destination);
+	UE_LOG(LogTemp, Log, TEXT("Unidad movida a (%d,%d)"), Target.X, Target.Y);
+
+	return true;
+}
---
+++ Egnis/Source/Egnis/Public/GridMovementComponent.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Board.h"
+#include "GridMovementComponent.generated.h"
+
+
+/*
+ * Para prototipo partimos de un mov de 8 dir como base ( primer personaje)
+ **/
+UENUM(BlueprintType)
+enum class EMovePattern : uint8
+{
+	Straight_WBlock UMETA(DisplayName="Recto 8 dir con bloaqueo"),
+	Straight_CanPass UMETA(DisplayName="Recto 8 dir sin bloqueo")//ignora obstaculos
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class EGNIS_API UGridMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGridMovementComponent();
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Movement")
+	int32 moveRange=4;//maximo de casillas a recorrer (oso)
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Movement")
+	EMovePattern movePattern=EMovePattern::Straight_WBlock;//patron actual
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Movement")
+	TObjectPtr<ABoard>BoardActor=nullptr;//ref para el tablero
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool getCurrentTile(FTileCoord& OutTile);//devuelve la casilla actual
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	void getRachableTiles(TArray<FTileCoord>& OutTiles);//calcula las casillas segun el patron y rango de mov
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool canMoveToTile(const FTileCoord& Target);//mira si la casilla esta dentro de los limites
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool moveToTile(const FTileCoord& Target);//nos devuelve true si se mueve
+
+private:
+	void EnsureBoardActor();//comprueba que este
+	bool IsTileValid(const FTileCoord& T);//mira que este dentro de limites tablero
+
+	
+protected:
+	
+};
