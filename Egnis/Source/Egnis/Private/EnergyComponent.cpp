9d2d73869d5ae9ff8892d52e15f39c2fcf10b6e6
trudytru30 <adriana.alamoclase@gmail.com>
2026-02-26 17:58:47 +0100

    EnergyComponent
    Puntos para energia de los jugadores/enemigos

---
+++ Egnis/Source/Egnis/Private/EnergyComponent.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnergyComponent.h"
+
+
+// Sets default values for this component's properties
+UEnergyComponent::UEnergyComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UEnergyComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UEnergyComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
